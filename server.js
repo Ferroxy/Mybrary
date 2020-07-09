b39ab213a5e1193f7993896595afe0c824e0dcc3
Ferhat TEKKOUK <38411423+Ferroxy@users.noreply.github.com>
2020-07-09 19:04:53 +0100

    Initial Author Route

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 if (process.env.NODE_ENV !== 'production') {
-    // require('dotenv').load() // A l'origine
     require('dotenv').config()
 }
 
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
+
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
@@ -15,24 +17,19 @@ app.set('layout', 'layouts/layout')
 
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 const mongoose = require('mongoose')
-// mongoose.connect('mongodb://localhost/mybrary', {
 mongoose.connect(process.env.DATABASE_URLL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
 
-// mongoose.connect('mongodb://localhost/mybrary', {
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true
-//     })
-//     .then(() => console.log('MongoDB Connected...'))
-//     .catch((err) => console.log(err))
-
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
+
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000) 
