6dd480d3b3eee05357a496a9abaffe15c642b8b5
Ferhat TEKKOUK <38411423+Ferroxy@users.noreply.github.com>
2020-07-13 04:16:44 +0100

    Retouches au niveau de views/authors/show.ejs

--- routes/authors.js
+++ routes/authors.js
@@ -11,9 +11,9 @@ router.get('/', async (req, res) => {
     }
     try {
         const authors = await Author.find(searchOptions)
-        res.render('authors/index', { 
-            authors: authors, 
-            searchOptions: req.query 
+        res.render('authors/index', {
+            authors: authors,
+            searchOptions: req.query
         })
     } catch {
         res.redirect('/')
@@ -22,7 +22,9 @@ router.get('/', async (req, res) => {
 
 // New Author Route
 router.get('/new', (req, res) => {
-    res.render('authors/new', { author: new Author() })
+    res.render('authors/new', {
+        author: new Author()
+    })
 })
 
 // Create Author Route
@@ -33,7 +35,7 @@ router.post('/', async (req, res) => {
     try {
         const newAuthor = await author.save()
         res.redirect(`authors/${newAuthor.id}`)
-    } catch (error) {
+    } catch {
         res.render('authors/new', {
             author: author,
             errorMessage: 'Error creating Author'
@@ -44,26 +46,27 @@ router.post('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
     try {
         const author = await Author.findById(req.params.id)
-        const books = await Book.find({ author: author.id }).limit(6).exec()
+        const books = await Book.find({
+            author: author.id
+        }).limit(6).exec()
         res.render('authors/show', {
             author: author,
             booksByAuthor: books
         })
     } catch {
         res.redirect('/')
-    } 
+    }
 })
 
 router.get('/:id/edit', async (req, res) => {
     try {
         const author = await Author.findById(req.params.id)
         res.render('authors/edit', {
-                author: author
-            })
+            author: author
+        })
     } catch {
         res.redirect('/authors')
     }
-    
 })
 
 router.put('/:id', async (req, res) => {
@@ -76,11 +79,10 @@ router.put('/:id', async (req, res) => {
     } catch {
         if (author == null) {
             res.redirect('/')
-        }
-        else {
+        } else {
             res.render('authors/edit', {
-            author: author,
-            errorMessage: 'Error updating Author'
+                author: author,
+                errorMessage: 'Error updating Author'
             })
         }
     }
